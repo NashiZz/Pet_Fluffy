a070a59ebd31dc8d8b5506f7922a105fa71b60f4
preeZa <115576549+preeZa@users.noreply.github.com>
2024-08-08 02:23:11 +0700

    notifuntion_sucss

--- lib/features/services/notification_helper.dart
+++ lib/features/services/notification_helper.dart
@@ -1,24 +1,81 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart'as tz;
+import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest_all.dart' as tz;
 
-class NotificationHelper{
+class NotificationHelper {
   static final _notification = FlutterLocalNotificationsPlugin();
 
   static init() {
     _notification.initialize(const InitializationSettings(
-      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
-      iOS: DarwinInitializationSettings()
-    ));
+        android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+        iOS: DarwinInitializationSettings()));
     tz.initializeTimeZones();
   }
 
-  static scheduledNotification (String title,String body,String date) async {
-    var androidDetails = AndroidNotificationDetails('important_notification' , 'My Channel',importance: Importance.max,priority: Priority.high);
+  static Future<void> scheduledNotification(
+      String title, String body, String date, String pet_type ) async {
+    if (pet_type == 'สุนัข') {
+      final DateTime now = DateTime.now();
+      DateTime dateSend = DateTime.parse(date);
+      DateTime newDateSend = dateSend.add(Duration(days: 2));
+      if (newDateSend.isAfter(now)) {
+        Duration difference = newDateSend.difference(now);
+        int daysDifference = difference.inDays;
 
-    var iosDetails = const DarwinNotificationDetails();
-    var notificationDetails = NotificationDetails(android: androidDetails,iOS: iosDetails);
-    await _notification.zonedSchedule(0, title, body, tz.TZDateTime.now(tz.local).add(const Duration(seconds: 10)), notificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime
-    ,androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle);
+        if (daysDifference <= 0) {
+          int daylate = daysDifference.abs();
+          print(daylate);
+          
+        } else {
+          var androidDetails = AndroidNotificationDetails(
+              'important_notification', 'My Channel',
+              importance: Importance.max, priority: Priority.high);
+
+          var iosDetails = const DarwinNotificationDetails();
+          var notificationDetails =
+              NotificationDetails(android: androidDetails, iOS: iosDetails);
+          await _notification.zonedSchedule(
+              0,
+              title,
+              body,
+              tz.TZDateTime.now(tz.local).add(Duration(days: daysDifference)),
+              notificationDetails,
+              uiLocalNotificationDateInterpretation:
+                  UILocalNotificationDateInterpretation.absoluteTime,
+              androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle);
+        }
+      }
+    } else {
+      final DateTime now = DateTime.now();
+      DateTime dateSend = DateTime.parse(date);
+      DateTime newDateSend = dateSend.add(Duration(days: 3));
+      if (newDateSend.isAfter(now)) {
+        Duration difference = newDateSend.difference(now);
+        int daysDifference = difference.inDays;
+
+        if (daysDifference <= 0) {
+          int daylate = daysDifference.abs();
+          print(daylate);
+          
+        } else {
+          var androidDetails = AndroidNotificationDetails(
+              'important_notification', 'My Channel',
+              importance: Importance.max, priority: Priority.high);
+
+          var iosDetails = const DarwinNotificationDetails();
+          var notificationDetails =
+              NotificationDetails(android: androidDetails, iOS: iosDetails);
+          await _notification.zonedSchedule(
+              0,
+              title,
+              body,
+              tz.TZDateTime.now(tz.local).add(Duration(days: daysDifference)),
+              notificationDetails,
+              uiLocalNotificationDateInterpretation:
+                  UILocalNotificationDateInterpretation.absoluteTime,
+              androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle);
+        }
+      }
+    }
   }
 }
